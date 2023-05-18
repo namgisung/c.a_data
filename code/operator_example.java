3540d1bb132344580bed07194a9fa149b6fd3077
namgisung <109130108+namgisung@users.noreply.github.com>
2023-05-19 02:58:42 +0900

    Create operator_example.java

---
+++ code/operator_example.java
@@ -0,0 +1,13 @@
+public class operator_example {
+    public static void main(String[] args){
+        double a = 10;
+        double b = 20;
+        double p,s,m,d,tmp = 0;
+        p = a+b;
+        s = a-b;
+        m = a*b;
+        d = a/b;
+        tmp = Math.pow(a , 3);
+        System.out.println(a+" "+b+"\n"+p+"\n"+s+"\n"+m+"\n"+d+"\n"+tmp);
+    }
+}
