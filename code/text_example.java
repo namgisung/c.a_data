b3fcaca7f7295031dbf833a76b79b0cd3bb297a6
namgisung <109130108+namgisung@users.noreply.github.com>
2023-05-19 02:55:35 +0900

    Create text_example.java

---
+++ code/text_example.java
@@ -0,0 +1,12 @@
+public class text_example {
+    public static void main(String[] args) {
+        /*String이나 char로 문자열을 저장하는 변수를 선언할 수 있다. 이 변수는 문자와 문자, 숫자와 문자를 덧셈 연산자로 합칠수 있다
+        char은 한 문자만 저장한다 ex) char t = 'r';
+         */
+        String name = "CPU";
+        char t = 'r';
+        String name1 = "동아리";
+        String text = 21109 + "남기성";
+        System.out.println(name+name1+"\n"+text+"\n"+t);
+    }
+}
