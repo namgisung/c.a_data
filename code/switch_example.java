2621ad1f0770218320b9c7675923785194615afa
namgisung <109130108+namgisung@users.noreply.github.com>
2023-05-19 03:00:32 +0900

    Create switch_example.java

---
+++ code/switch_example.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class switch_example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자 입력(0 < x < 3): ");
+        //scanner로 입력을 받아 문자를 저장하는 변수 input을 선언하여 저장한다
+        String input = scanner.nextLine();
+        //문자 형태인 input을 정수형태로 바꾼다
+        int number = Integer.parseInt(input);
+
+        switch (number) {
+            case 1:
+                System.out.println("1을 입력했군");
+            case 2:
+                System.out.println("2를 입력했군");
+            default:
+                System.out.println("멍청하군");
+        }
+    }
+}
