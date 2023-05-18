08ea1c94da0ec8ea43cdfc12b28b902e5d697dc3
namgisung <109130108+namgisung@users.noreply.github.com>
2023-05-19 02:54:24 +0900

    Create double_example.java

---
+++ code/double_example.java
@@ -0,0 +1,9 @@
+public class double_example {
+    public static void main(String[] arg) {
+        //double 또는 float를 사용하여 실수를 저장하는 변수를 선언한다
+        double x = 3.14;
+
+
+        System.out.println(x);
+    }
+}
