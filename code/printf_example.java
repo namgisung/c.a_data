54e2d3e8990a78334c87933f5d220c28d6c155a1
namgisung <109130108+namgisung@users.noreply.github.com>
2023-05-19 02:56:41 +0900

    Create printf_example.java

---
+++ code/printf_example.java
@@ -0,0 +1,16 @@
+public class printf_example {
+    public static void main(String[] args){
+        int a = 3;
+        double b= 3.14;
+        String str = "안녕";
+        char c = 'a';
+        System.out.printf("a = %d%n", a);
+        System.out.printf("b = %f%n", b);
+        System.out.printf("나는 %s%n", str);
+        System.out.printf("a = %c", c);
+
+
+
+
+    }
+}
