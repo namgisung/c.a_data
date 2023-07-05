d80eceb47c210e253929b51c65a68febda1349de
namgisung <109130108+namgisung@users.noreply.github.com>
2023-07-05 13:36:47 +0900

    Create arr _example.java

---
+++ code/arr _example.java
@@ -0,0 +1,15 @@
+import java.util.Arrays; // Arrays.toString()을 사용하기 위한 import
+
+class Test{
+	public static void main(String[] args) {
+        int[] iArr = {100, 95, 80, 70, 60};
+        
+        // 루프문으로 직접 배열 원소 출력
+        for(int i = 0 ; i < iArr.length; i++){
+            System.out.println(iArr[i]);
+        }
+
+        // Arrays.toString() 메서드 사용하여 심플하게 바로 출력
+        System.out.println(Arrays.toString(iArr)); // [100, 95, 80, 70, 60]
+	}
+}
