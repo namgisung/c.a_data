43121d5a999203a157fb70cfc7b8bc13a3f5c904
namgisung <109130108+namgisung@users.noreply.github.com>
2023-07-07 01:34:12 +0900

    Create arr_remove_example.java

---
+++ code/arr_remove_example.java
@@ -0,0 +1,11 @@
+public static void Arr_remove(String args[]) {
+  String[] strArray = { "10", "20", "30", "40", "50", "60", "70" };
+
+  System.out.println("2번째 인덱스의 요소 삭제 전: " + Arrays.toString(strArray));
+
+  List<String> strList = new ArrayList<>(Arrays.asList(strArray));
+  strList.remove(2);
+  strArray = strList.toArray(new String[0]);
+
+  System.out.println("2번째 인덱스의 요소 삭제 후: " + Arrays.toString(strArray));
+}
