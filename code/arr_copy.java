fcf9fd4e1a16b33fda2fdd2f629744e021000bb5
namgisung <109130108+namgisung@users.noreply.github.com>
2023-07-07 11:41:11 +0900

    Create arr_copy.java

---
+++ code/arr_copy.java
@@ -0,0 +1,16 @@
+import java.util.Arrays; // Arrays.toString()을 사용하기 위한 import
+
+class Arr_int{
+	public static void main(String[] args) {
+    in[] arr = new int[5];
+
+    for(int i=0; i < arr.length;i++)
+      arr[i] = i + 1;
+
+    System.out.println("변경전 - arr.length"+arr.length);
+    for(int i=0; i < arr.length;i++)
+      System.out.println("arr["+arr.length);
+    int[] tmp = new int[arr.length*2];
+    
+  }
+}
