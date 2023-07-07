9720e31135bf7fbe6e8b2721fe1d66504bd621fe
namgisung <109130108+namgisung@users.noreply.github.com>
2023-07-07 14:49:53 +0900

    Update arr_copy.java

--- code/arr_copy.java
+++ code/arr_copy.java
@@ -15,7 +15,8 @@ class Arr_int{
 	    tmp[i] = arr[i];
 
     arr = tmp;
-    System.out.println()
-    
+    System.out.println("변경후 - arr.length:"+arr.length)
+    for(int i=0; i < arr.length;i++)
+	    System.out.println("arr["+i+"]:"+arr[i])
   }
 }
