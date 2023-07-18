4413eb5f4a2adc65b9b7db414e1824e2e57f1cc4
namgisung <109130108+namgisung@users.noreply.github.com>
2023-07-18 21:47:07 +0900

    Update arr_copy.java

--- code/arr_copy.java
+++ code/arr_copy.java
@@ -1,22 +1,22 @@
-import java.util.Arrays; // Arrays.toString()을 사용하기 위한 import
+public class arr_copy_example {
 
-class Arr_int{
-	public static void main(String[] args) {
-    in[] arr = new int[5];
+        public static void main(String[] args) {
+            int[] arr = new int[5];
 
-    for(int i=0; i < arr.length;i++)
-      arr[i] = i + 1;
+            for(int i=0; i < arr.length;i++)
+                arr[i] = i + 1;
 
-    System.out.println("변경전 - arr.length"+arr.length);
-    for(int i=0; i < arr.length;i++)
-      System.out.println("arr["+i+"]:"+arr[i]);
-    int[] tmp = new int[arr.length*2];
-    for(int i = 0; i < arr.length; i++)
-	    tmp[i] = arr[i];
+            System.out.println("변경전 - arr.length"+arr.length);
+            for(int i=0; i < arr.length;i++)
+                System.out.println("arr["+i+"]:"+arr[i]);
+            int[] tmp = new int[arr.length*2];
+            for(int i = 0; i < arr.length; i++)
+                tmp[i] = arr[i];
 
-    arr = tmp;
-    System.out.println("변경후 - arr.length:"+arr.length)
-    for(int i=0; i < arr.length;i++)
-	    System.out.println("arr["+i+"]:"+arr[i])
-  }
+            arr = tmp;
+            System.out.println("변경후 - arr.length:"+arr.length);
+            for(int i=0; i < arr.length;i++)
+                System.out.println("arr["+i+"]:"+arr[i]);
+        }
+
 }
